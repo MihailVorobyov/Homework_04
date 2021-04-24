b8e6b2ac20edc85c81e4cbf98385f5d57a0df9cf
Mihail <ptichkinm@mail.ru>
2021-04-24 22:10:25 +0300

    Done

--- src/PrintABC.java
+++ src/PrintABC.java
@@ -3,5 +3,73 @@
 // Используйте wait/notify/notifyAll.
 
 public class PrintABC {
+	private final Object mon = new Object();
+	private volatile char currentLetter = 'A';
 
+	public static void main(String[] args) {
+		PrintABC printABC = new PrintABC();
+		Thread thread1 = new Thread(() -> {
+			printABC.printA();
+		});
+		Thread thread2 = new Thread(() -> {
+			printABC.printB();
+		});
+		Thread thread3 = new Thread(() -> {
+			printABC.printC();
+		});
+		thread1.start();
+		thread2.start();
+		thread3.start();
+	}
+
+	public void printA() {
+		synchronized (mon) {
+			try {
+				for (int i = 0; i < 5; i++) {
+					while (currentLetter != 'A') {
+						mon.wait();
+					}
+					System.out.print("A");
+					currentLetter = 'B';
+					mon.notifyAll();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void printB() {
+		synchronized (mon) {
+			try {
+				for (int i = 0; i < 5; i++) {
+					while (currentLetter != 'B') {
+						mon.wait();
+					}
+					System.out.print("B");
+					currentLetter = 'C';
+					mon.notifyAll();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void printC() {
+		synchronized (mon) {
+			try {
+				for (int i = 0; i < 5; i++) {
+					while (currentLetter != 'C') {
+						mon.wait();
+					}
+					System.out.print("C");
+					currentLetter = 'A';
+					mon.notifyAll();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
